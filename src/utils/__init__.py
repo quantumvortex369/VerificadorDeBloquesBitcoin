eabeaf8d09b1347fd8edef60f4a6060bbe3fc0e1
N3X2S <xyzxyzxyz12345lol@gmail.com>
2025-09-25 23:45:00 +0200

    Create __init__.py

---
+++ src/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Módulo de utilidades para el Verificador de Bloques Bitcoin.
+
+Este paquete contiene funciones de utilidad que pueden ser utilizadas
+por diferentes partes de la aplicación.
+"""
+
+__all__ = []  # Lista de módulos disponibles para importación
